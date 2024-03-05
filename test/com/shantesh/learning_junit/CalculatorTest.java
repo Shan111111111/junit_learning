c54cf7a170ac78fcb5de3fb1d4d127f1b1cd62cc
Shantesh <shanthu1804@gmail.com>
2024-03-06 02:35:47 +0530

    added First Testcase

---
+++ test/com/shantesh/learning_junit/CalculatorTest.java
@@ -0,0 +1,17 @@
+package com.shantesh.learning_junit;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class CalculatorTest {
+
+	@Test
+	void nowTestingThisone() {
+		Calculator calculator = new Calculator();
+		int result = calculator.add(new int[] {1,2,4,3});
+		assertEquals(10, result, "assert Failed Uffff");
+		
+	}
+
+}
